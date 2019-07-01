2bce95e40f51bc3c2d263f37a391ec52deb88596
Yoshiapolis <yoshiapolis@gmail.com>
2019-07-01 10:16:30 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -37,7 +37,7 @@ AutoPlay.tryBestBuy = function() {
 	index = 0;
 	
 	for(i = 0; i < upgradePP.length; i ++) {
-		if(upgradePP[i] < minUpgradePP) {
+		if(upgradePP[i] < minUpgradePP && Game.UpgradesById[i].unlocked) {
 			bestUpgradeIndex = index;
 			minUpgradePP = upgradePP[i];
 		}
