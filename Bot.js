e839402a9a417b688833f86b71b453433f189dfa
Yoshiapolis <yoshiapolis@gmail.com>
2019-07-01 11:59:13 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,7 +1,10 @@
 var AutoPlay = {};
 var buildingPP = [];	
 var upgradePP = [];
+var clickyUpgrades
 var nBuildingTypes = 3;
+var cookiesPC;
+var autoclickCPS = 50;
 
 AutoPlay.calculatePP = function() {
 	
@@ -14,11 +17,53 @@ AutoPlay.calculatePP = function() {
 	iters = 0;
 	
 	for(var i in CM.Cache.Upgrades) { 
+		if(i == "Ambidextrous") {
+			var bonus = 2*autoclickCPS;
+			CM.Cache.Upgrades[i].pp = (Math.max(CM.Cache.Upgrades[i].cost-Game.cookies, 0)/Game.cookiesPs) + CM.Cache.Upgrades[i].cost/bonus; 
+		}
 		upgradePP[iters] = CM.Cache.Upgrades[i].pp;
-		iters ++;	
+		console.log(CM.Cache.Upgrades[i].
+		iters ++;
 	}
 }
 
+AutoPlay.updateCpC = function() {
+	var CpC = 1;
+	
+			
+	if(Game.Has("Reinforced index finger")) CpC *= 2;
+	if(Game.Has("Carpal tunnel prevention cream")) CpC *= 2;
+	if(Game.Has("Ambidextrous")) CpC *= 2;
+	if(Game.Has("Thousand fingers")) CpC += 0.1 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Million fingers")) CpC += 0.5 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Billion fingers")) CpC += 5 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Trillion fingers")) CpC += 50 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Quadrillion fingers")) CpC += 500 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Quintillion fingers")) CpC += 5000 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Sextillion fingers")) CpC += 50000 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Septillion fingers")) CpC += 500000 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+	if(Game.Has("Octillion fingers")) CpC += 5000000 * (Game.BuildingsOwned - Game.ObjectsById[0].amount);
+		
+	var cpsMult = 1;
+			
+	if(Game.Has("Plastic mouse")) cpsMult += 0.01;
+	if(Game.Has("Iron mouse")) cpsMult += 0.01;
+	if(Game.Has("Titanium mouse")) cpsMult += 0.01;
+	if(Game.Has("Adamantium mouse")) cpsMult += 0.01;
+	if(Game.Has("Unobtainium mouse")) cpsMult += 0.01;
+	if(Game.Has("Eludium mouse")) cpsMult += 0.01;
+	if(Game.Has("Wishalloy mouse")) cpsMult += 0.01;
+	if(Game.Has("Fantasteel mouse")) cpsMult += 0.01;
+	if(Game.Has("Nevercrack mouse")) cpsMult += 0.01;
+	if(Game.Has("Armythril mouse")) cpsMult += 0.01;
+	if(Game.Has("Technobsidian mouse")) cpsMult += 0.01;
+	if(Game.Has("Plasmarble mouse")) cpsMult += 0.01;
+			
+	CpC += cpsMult * Game.cookiesPs;
+			
+	this.cookiesPC = CpC;
+}
+			    
 AutoPlay.tryBestBuy = function() {
 	var minPP = buildingPP[0];
 	var bestIndex = 0;
