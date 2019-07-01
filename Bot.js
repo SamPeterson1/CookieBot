58a93060a9276f7511bb9b9e1354cffd79cdc553
Yoshiapolis <yoshiapolis@gmail.com>
2019-06-30 22:09:24 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,17 +1,20 @@
 var AutoPlay = {};
 var buildingPP = [];		  
+var nBuildingTypes = 3;
 
 AutoPlay.calculatePP = function() {
-	var building = Game.ObjectsById[AutoPlay.cursorCPS()-1];
-	building.buy();
-}
-
-AutoPlay.cursorCPS = function() {
-	return 1;
+	
+	var iters = 0;
+	for(var i in CM.Cache.Objects) { 
+		buildingPP[iters] = CM.Cache.Objects[i].pp;
+		console.log(buildingPP[iters]);
+		iters ++;
+	}
 }
 
 AutoPlay.run = function() {
 	AutoPlay.calculatePP();
 }
 
-AutoPlay.autoPlayer = setInterval(AutoPlay.run, 300);
+
+AutoPlay.autoPlayer = setInterval(AutoPlay.run, 10000);
