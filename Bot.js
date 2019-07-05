f36493de00b29c8581f318b64a3036a4f243e6a7
Yoshiapolis <yoshiapolis@gmail.com>
2019-07-04 22:38:14 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -37,15 +37,7 @@ AutoPlay.calculatePP = function() {
 		if(i == "Sextillion fingers") CM.Cache.Upgrades[i].pp = AutoPlay.UpgradePP(i, (50000*(Game.BuildingsOwned-Game.ObjectsById[0].amount))*(autoclickCPS+Game.ObjectsById[0].amount));
 		if(i == "Septillion fingers") CM.Cache.Upgrades[i].pp = AutoPlay.UpgradePP(i, (500000*(Game.BuildingsOwned-Game.ObjectsById[0].amount))*(autoclickCPS+Game.ObjectsById[0].amount));
 		if(i == "Octillion fingers") CM.Cache.Upgrades[i].pp = AutoPlay.UpgradePP(i, (5000000*(Game.BuildingsOwned-Game.ObjectsById[0].amount))*(autoclickCPS+Game.ObjectsById[0].amount));
-		
-		for(ii = 0; ii < Game.UpgradesInStore.length; ii ++) {
-			if(Game.UpgradesInStore[ii].name == i) {
-				upgradeNames[iters] = i;
-				upgradePP[iters] = CM.Cache.Upgrades[i].pp;
-				iters ++;
-			}
-		}
-		
+				
 		if(i == "Plastic mouse") upgradePP[iters] = AutoPlay.UpgradePP(i, 0.01*Game.cookiesPs*autoclickCPS);
 		if(i == "Iron mouse") upgradePP[iters] = AutoPlay.UpgradePP(i, 0.01*Game.cookiesPs*autoclickCPS);
 		if(i == "Titanium mouse") upgradePP[iters] = AutoPlay.UpgradePP(i, 0.01*Game.cookiesPs*autoclickCPS);
@@ -59,7 +51,13 @@ AutoPlay.calculatePP = function() {
 		if(i == "Technobsidian mouse") upgradePP[iters] = AutoPlay.UpgradePP(i, 0.01*Game.cookiesPs*autoclickCPS);
 		if(i == "Plasmarble mouse") upgradePP[iters] = AutoPlay.UpgradePP(i, 0.01*Game.cookiesPs*autoclickCPS);
 		
-
+		for(ii = 0; ii < Game.UpgradesInStore.length; ii ++) {
+			if(Game.UpgradesInStore[ii].name == i) {
+				upgradeNames[iters] = i;
+				upgradePP[iters] = CM.Cache.Upgrades[i].pp;
+				iters ++;
+			}
+		}
 	}
 }
 
