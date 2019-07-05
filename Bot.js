607731025fc459e5c9a2035abee8e60d216d5113
Yoshiapolis <yoshiapolis@gmail.com>
2019-07-05 15:36:27 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -286,7 +286,7 @@ AutoPlay.upgradeGains = function(name) {
 		if(name.includes("grandmas")) {
 			var buildingId = Game.Upgrades[name].buildingTie.id;
 			var buildingName = Game.Upgrades[name].buildingTie.name;
-			var benefit = Math.floor(Game.ObjectsById[1]/(buildingId-1))*0.01;
+			var benefit = Math.floor(Game.ObjectsById[1].amount/(buildingId-1))*0.01;
 			console.log(benefit + " " + AutoPlay.getMult(buildingName));
 			return AutoPlay.getCps(1) + AutoPlay.getCps(buildingId)/AutoPlay.getMult(buildingName)*(AutoPlay.getMult(buildingName)+benefit);
 		}
