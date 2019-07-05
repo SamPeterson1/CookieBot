4ed4030bf7700f52bfc6050d898569f77c88e244
Yoshiapolis <yoshiapolis@gmail.com>
2019-07-05 11:34:30 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -262,7 +262,12 @@ AutoPlay.upgradeGains = function(name) {
 	}
 	
 	if(desc.includes("The mouse and cursors are <b>twice</b> as efficient") {
-		return AutoPlay.getCps(0) + AutoPlay.
+		return AutoPlay.getCps(0) + AutoPlay.cookiesPC;
+	}
+	if(desc.includes("The mouse and cursors gain")) {
+		var num = parseInt(desc.split("<b>")[1].split("</b>")[0], 10);
+		var nonCursor = Game.buildingsOwned - Game.ObjectsById[0].amount;
+		return nonCursor * num;
 	}
 }
 
